cb91276bcfd3da48e0e74a7d0ef9040dd88af046
AbinavRavi <abinav.ravi@tum.de>
2023-08-10 22:35:06 +0530

    inference is done needs to be tested

--- app/features/inference.py
+++ app/features/inference.py
@@ -1,28 +1,22 @@
-import transformers
 import torch
-from transformers import AutoTokenizer
-from transformers import pipeline
-
-name = "mosaicml/mpt-7b-chat"
-
-config = transformers.AutoConfig.from_pretrained(name, trust_remote_code=True)
-config.max_seq_len = 4096  # (input + output) tokens can now be up to 4096
-
-model = transformers.AutoModelForCausalLM.from_pretrained(
-    name, config=config, trust_remote_code=True
-)
-
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+from transformers import LlamaForCausalLM, LlamaTokenizer
 
 
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device="cuda:0")
-
-with torch.autocast("cuda", dtype=torch.bfloat16):
-    print(
-        pipe(
-            "Here is a recipe for vegan banana bread:\n",
-            max_new_tokens=100,
-            do_sample=True,
-            use_cache=True,
+class Inference:
+    def __init__(self) -> None:
+        self.snapshot_path = "./models"
+        self.model = LlamaForCausalLM.AutoModelForCausalLM.from_pretrained(
+            self.snapshot_path,
+            quantized=True,
+            torch_dtype=torch.bfloat16,
+            low_cpu_mem_usage=True,
+            trust_remote_code=True,
         )
-    )
+        self.tokenizer = LlamaTokenizer.AutoTokenizer.from_pretrained(self.snapshot_path)
+
+    def generate(self, prompt: str) -> str:
+        input_ids = self.tokenizer.encode(prompt, return_tensors="pt")
+        output = self.model.generate(input_ids, max_length=100)
+        # Convert the generated output back to text
+        generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
+        return generated_text
