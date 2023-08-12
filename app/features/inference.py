b66e3118fda9866ef642014f0ef89b9a41b26c31
AbinavRavi <abinav.ravi@tum.de>
2023-08-12 06:37:38 +0530

    need a rewrite to use llama cpp python bindings

--- app/features/inference.py
+++ app/features/inference.py
@@ -5,14 +5,14 @@ from transformers import LlamaForCausalLM, LlamaTokenizer
 class Inference:
     def __init__(self) -> None:
         self.snapshot_path = "./models"
-        self.model = LlamaForCausalLM.AutoModelForCausalLM.from_pretrained(
+        self.model = LlamaForCausalLM.from_pretrained(
             self.snapshot_path,
             quantized=True,
             torch_dtype=torch.bfloat16,
             low_cpu_mem_usage=True,
             trust_remote_code=True,
         )
-        self.tokenizer = LlamaTokenizer.AutoTokenizer.from_pretrained(self.snapshot_path)
+        self.tokenizer = LlamaTokenizer.from_pretrained(self.snapshot_path)
 
     def generate(self, prompt: str) -> str:
         input_ids = self.tokenizer.encode(prompt, return_tensors="pt")
--- app/infer.py
+++ app/infer.py
@@ -1,27 +1,21 @@
 from pydantic import BaseModel
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from app.features.inference import Inference
 
 
-class InputModel(BaseModel):
-    new_input: str
-    context: str
+class Input(BaseModel):
+    prompt: str
+
+
+class Output(BaseModel):
+    response: str
 
 
 app = FastAPI()
 
 
-@app.post("/chat")
-def respond(input: InputModel, request: Request):
-    pass
-
-
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-
-pipe = pipeline("text-generation", model="meta-llama/Llama-2-7b-chat-hf")
-
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+@app.post("/chat", response_model=Output)
+def respond(input: Input):
+    model = Inference()
+    output = model.generate(input.prompt)
+    return {"response": output}
